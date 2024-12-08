dee5ab799739e898dff3fba3feee76e5378ba860
Raudel Alejandro Gomez Molina <raudel.gmolina@gmail.com>
2024-12-08 16:29:58 -0500

    first commit

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  ThemeData get theme => ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: const Color(0xFF2862f5),
+      );
+}
